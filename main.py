d8cecfc2722e3e79fbf05bae03d6a805241b9ffb
thushara@codecommit-at-789734661575 <LAo4RY13QCYEqifAdF8HEUHsQvfvyPyYdACB+jhsUY8=>
2021-10-02 23:57:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from inner_module import lib_functions
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    lib_functions.lib_func_one()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
